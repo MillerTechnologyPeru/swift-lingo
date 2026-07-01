eb2f0b549062e54ce8faf7c9479b10f13580d91d
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-06-30 21:46:40 -0400

    Add `Equatable` conformance to `LingoSyntax`

--- Sources/LingoAST/LingoSyntax.swift
+++ Sources/LingoAST/LingoSyntax.swift
@@ -4,7 +4,7 @@
 ///
 /// - `verbose`: `set the crop of member "x" to TRUE`, `word 2 of paragraph 1 of member("x")`
 /// - `dot`: `member("x").crop = TRUE`, `member("x").paragraph[1].word[2]`
-public enum LingoSyntax: Sendable, CaseIterable {
+public enum LingoSyntax: Sendable, CaseIterable, Equatable {
     case verbose
     case dot
 }
