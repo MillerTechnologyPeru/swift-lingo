d1011c738314399561be1e5d72e0aba256e4a6ad
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-06-30 21:34:49 -0400

    Add `LingoSyntax`

---
+++ Sources/LingoAST/LingoSyntax.swift
@@ -0,0 +1,10 @@
+/// The two textual dialects Director's Lingo scripting dictionary documents as
+/// equivalent: the older sentence-like form and the object-oriented form
+/// introduced to mirror JavaScript.
+///
+/// - `verbose`: `set the crop of member "x" to TRUE`, `word 2 of paragraph 1 of member("x")`
+/// - `dot`: `member("x").crop = TRUE`, `member("x").paragraph[1].word[2]`
+public enum LingoSyntax: Sendable, CaseIterable {
+    case verbose
+    case dot
+}
