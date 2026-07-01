5662d3239accf5d2ff786054aba40500dc2c4692
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-06-30 20:31:51 -0400

    Optimize `String` extensions

--- Sources/LingoRuntime/Extensions/String.swift
+++ Sources/LingoRuntime/Extensions/String.swift
@@ -23,59 +23,67 @@ extension String {
     }
 
     public func caseInsensitiveEquals(_ other: String) -> Bool {
-        let lUTF8 = self.utf8
-        let rUTF8 = other.utf8
-        if lUTF8.count != rUTF8.count { return false }
-        var lIter = lUTF8.makeIterator()
-        var rIter = rUTF8.makeIterator()
-        while let lByte = lIter.next(), let rByte = rIter.next() {
-            if _asciiLowercased(lByte) != _asciiLowercased(rByte) {
-                return false
+        if self.utf8.count != other.utf8.count { return false }
+        return self.withUTF8 { lBuf in
+            other.withUTF8 { rBuf in
+                if lBuf.count != rBuf.count { return false }
+                for i in 0..<lBuf.count {
+                    if _asciiLowercased(lBuf[i]) != _asciiLowercased(rBuf[i]) {
+                        return false
+                    }
+                }
+                return true
             }
         }
-        return true
     }
 
     public func caseInsensitiveLessThan(_ other: String) -> Bool {
-        var lIter = self.utf8.makeIterator()
-        var rIter = other.utf8.makeIterator()
-        while let lByte = lIter.next() {
-            guard let rByte = rIter.next() else { return false }  // r is shorter
-            let lLower = _asciiLowercased(lByte)
-            let rLower = _asciiLowercased(rByte)
-            if lLower != rLower {
-                return lLower < rLower
+        return self.withUTF8 { lBuf in
+            other.withUTF8 { rBuf in
+                let minCount = Swift.min(lBuf.count, rBuf.count)
+                for i in 0..<minCount {
+                    let lLower = _asciiLowercased(lBuf[i])
+                    let rLower = _asciiLowercased(rBuf[i])
+                    if lLower != rLower {
+                        return lLower < rLower
+                    }
+                }
+                return lBuf.count < rBuf.count
             }
         }
-        // l is shorter or equal
-        return rIter.next() != nil
     }
 
     public func caseInsensitiveContains(_ substr: String) -> Bool {
-        let sBytes = Array(self.utf8)
-        let subBytes = Array(substr.utf8)
-        if subBytes.isEmpty { return true }
-        if subBytes.count > sBytes.count { return false }
-        for i in 0...(sBytes.count - subBytes.count) {
-            var match = true
-            for j in 0..<subBytes.count {
-                if _asciiLowercased(sBytes[i + j]) != _asciiLowercased(subBytes[j]) {
-                    match = false
-                    break
+        if substr.isEmpty { return true }
+        return self.withUTF8 { sBuf in
+            substr.withUTF8 { subBuf in
+                if subBuf.count > sBuf.count { return false }
+                if subBuf.isEmpty { return true }
+                for i in 0...(sBuf.count - subBuf.count) {
+                    var match = true
+                    for j in 0..<subBuf.count {
+                        if _asciiLowercased(sBuf[i + j]) != _asciiLowercased(subBuf[j]) {
+                            match = false
+                            break
+                        }
+                    }
+                    if match { return true }
                 }
+                return false
             }
-            if match { return true }
         }
-        return false
     }
 
     public func caseInsensitiveStartsWith(_ prefix: String) -> Bool {
-        let sBytes = Array(self.utf8)
-        let pBytes = Array(prefix.utf8)
-        if pBytes.count > sBytes.count { return false }
-        for i in 0..<pBytes.count {
-            if _asciiLowercased(sBytes[i]) != _asciiLowercased(pBytes[i]) { return false }
+        return self.withUTF8 { sBuf in
+            prefix.withUTF8 { pBuf in
+                if pBuf.count > sBuf.count { return false }
+                if pBuf.isEmpty { return true }
+                for i in 0..<pBuf.count {
+                    if _asciiLowercased(sBuf[i]) != _asciiLowercased(pBuf[i]) { return false }
+                }
+                return true
+            }
         }
-        return true
     }
 }
